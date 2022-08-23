4c11a3ee6b7fceb739111e15a49778d576e0ba33
Aaron <aaronpena@gmail.com>
2022-08-23 16:38:10 -0700

    act5dos_checks_mas

--- cypress/integration/2-advanced-examples/casodePruebas5.js
+++ cypress/integration/2-advanced-examples/casodePruebas5.js
@@ -2,8 +2,9 @@ describe("quinto caso de prueba", function () {
 
   it("Verificar que los checkboxes estan funcionando", function () {
     cy.visit("https://www.amazon.com/");
-    cy.get('#nav-xshop > a:nth-child(2)').click()
+    cy.get('[href="/gp/goldbox?ref_=nav_cs_gb"]').click()
     cy.get('[aria-label="Prime free shipping filter"] > :nth-child(2) > label > .CheckboxFilter-module__gridFilterCheckbox_9gZBKxneWNZMc30ac9ue7').check().should('be.checked')
-
+    cy.get('[aria-label="Prime free shipping filter"] > :nth-child(4) > label > .CheckboxFilter-module__gridFilterCheckbox_9gZBKxneWNZMc30ac9ue7').check().should('be.checked')
+    cy.get('[aria-label="Prime free shipping filter"] > :nth-child(3) > label > .CheckboxFilter-module__gridFilterCheckbox_9gZBKxneWNZMc30ac9ue7').should('not.be.checked')
   });
 });
