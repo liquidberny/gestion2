e7b60b20f0ffe735f1fb8fb3b6943a3820673a66
Aaron <aaronpena@gmail.com>
2022-08-31 18:28:51 -0700

    31augotroValidacionHobbies

--- cypress/integration/2-advanced-examples/PruebasDataDriven.js
+++ cypress/integration/2-advanced-examples/PruebasDataDriven.js
@@ -23,11 +23,18 @@ describe("Segundo conjunto de casos de pruebas avanzadas", function () {
     cy.get('.react-datepicker__month-select').should('be.visible').select(this.datos.fechaDeNacimiento[0])
     cy.get('.react-datepicker__year-select').should('be.visible').select(this.datos.fechaDeNacimiento[1])
     cy.get('.react-datepicker__day--0'+this.datos.fechaDeNacimiento[2]).click()
-    
+   /* 
     this.datos.Hobbies[0] = 1
     cy.get('#hobbies-checkbox-'+this.datos.Hobbies[0]).check({force:true}).should('be.checked')
     this.datos.Hobbies[1] = 3
-    cy.get('#hobbies-checkbox-'+this.datos.Hobbies[1]).check({force:true}).should('be.checked')
+    cy.get('#hobbies-checkbox-'+this.datos.Hobbies[1]).check({force:true}).should('be.checked')*/
+    if(cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').contains(this.datos.Hobbies[0])){
+      cy.get('#hobbies-checkbox-1').check({force:true}).should('be.checked')
+    }
+    if(cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(3) > .custom-control-label').contains(this.datos.Hobbies[1])){
+      cy.get('#hobbies-checkbox-3').check().should('be.checked')
+    }
+
   });
 
 
